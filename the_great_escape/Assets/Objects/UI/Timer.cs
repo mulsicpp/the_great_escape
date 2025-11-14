e06be0ed18286cccceca29db08ae64fc6123a7aa
Lennert1 <lennertmay@gmail.com>
2025-11-14 16:19:03 +0100

    Added simple Timer UI

---
+++ the_great_escape/Assets/Objects/UI/Timer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    float elapsedTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        timerText.text = elapsedTime.ToString("0");
+    }
+}
