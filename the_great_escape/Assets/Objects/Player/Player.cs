0617787d83b7d918b5cebe0d5da077493d4786ec
mulsicpp <christianmulser000@gmail.com>
2025-11-16 16:02:55 +0100

    added health and invisible frames

--- the_great_escape/Assets/Objects/Player/Player.cs
+++ the_great_escape/Assets/Objects/Player/Player.cs
@@ -4,25 +4,33 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public SpriteRenderer sprite;
     public GameObject projectile;
     public uint ammo;
+    public uint health;
+
     const float MOVE_VELOCITY = 5.0f;
     const uint AMMO_PER_UNIT = 3;
+    const uint MAX_HEALTH = 3;
 
     private Vector2 target_velocity;
     private Vector2 shoot_target;
+    public float invisible_frames_time;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = rb.GetComponent<SpriteRenderer>();
         target_velocity = Vector2.zero;
         ammo = 0;
+        health = MAX_HEALTH;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        var target_velocity = this.target_velocity * (invisible_frames_time <= 0.0f ? 1.0f : 1.4f);
         var diff = target_velocity - rb.linearVelocity;
 
         var diff_norm = diff.normalized;
@@ -34,6 +42,12 @@ public class Player : MonoBehaviour
             rb.linearVelocity = rb.linearVelocity + jump_distance * diff_norm;
         else
             rb.linearVelocity = target_velocity;
+
+        invisible_frames_time = Mathf.Max(invisible_frames_time - Time.fixedDeltaTime, 0.0f);
+
+        var color = sprite.color;
+        color.a = Mathf.Cos(Mathf.Log(invisible_frames_time + 1.0f, 2) * 30.0f) * 0.5f + 0.5f;
+        sprite.color = color;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -77,6 +91,26 @@ public class Player : MonoBehaviour
         {
             Destroy(collision.gameObject);
             ammo += AMMO_PER_UNIT;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 6 /*Enemy*/)
+        {
+            Destroy(collision.gameObject);
+
+            if(health > 0 && invisible_frames_time <= 0.0f)
+            {
+                health--;
+                invisible_frames_time = 2.0f;
+            }
+            else {
+                // TODO game over
+                return;
+            }
+
+            invisible_frames_time = 2.0f;
         }
     }
 }
