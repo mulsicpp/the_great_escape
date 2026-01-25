cce0967af22fe7a7a98d5f8141b80810bdbaf6d8
mulsicpp <christianmulser000@gmail.com>
2026-01-25 22:25:46 +0100

    Enemy moves to start

--- the_greater_escape/Assets/Objects/Enemy/Enemy.cs
+++ the_greater_escape/Assets/Objects/Enemy/Enemy.cs
@@ -1,8 +1,28 @@
+using UnityEditor;
 using UnityEngine;
+
+public enum EnemyState
+{
+    Patroling,
+    Inspecting,
+    Chasing
+}
 
 public class Enemy : Entity
 {
     public MazeGrid maze_grid;
+
+    public const float PATROLING_SPEED = 0.5f;
+    public const float INSPECTING_SPEED = 0.8f;
+    public const float CHASE_SPEED = 1.2f;
+
+    TransformInterpolation interpolation;
+
+    public Vector3Int last_knwon_player_pos;
+    public NavGrid nav_grid;
+    public EnemyState state;
+
+    public float speed;
 
     void OnEnable()
     {
@@ -13,14 +33,73 @@ public class Enemy : Entity
             up = Vector3Int.up
         };
 
+        interpolation = null;
+
         transform.localPosition = player_transform.Position();
         transform.localRotation = player_transform.Rotation().rotation;
+
+        last_knwon_player_pos = Vector3Int.zero;
+        state = EnemyState.Patroling;
+    }
+
+    public static int Dot(Vector3Int v1, Vector3Int v2)
+    {
+        return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        switch(state)
+        {
+            case EnemyState.Patroling:
+                speed = PATROLING_SPEED; break;
+            case EnemyState.Inspecting:
+                speed = INSPECTING_SPEED; break;
+            default: 
+                speed = CHASE_SPEED; break;
+        }
+        if (interpolation == null)
+        {
+            nav_grid = maze_grid.NavigateTo(last_knwon_player_pos);
 
+            var dir = nav_grid.GetDirection(player_transform.grid_position);
+            switch (Dot(dir, player_transform.forward))
+            {
+                case 1:
+                    interpolation = new MoveForwardInterpolation();
+                    break;
+                case -1:
+                    interpolation = new RotateRightInterpolation();
+                    break;
+                default:
+                    switch (Dot(dir, player_transform.up))
+                    {
+                        case 1:
+                            interpolation = new RotateUpInterpolation();
+                            break;
+                        case -1:
+                            interpolation = new RotateDownInterpolation();
+                            break;
+                        default:
+                            if (Dot(dir, player_transform.calc_right()) == 1)
+                            {
+                                interpolation = new RotateRightInterpolation();
+                            }
+                            else
+                            {
+                                interpolation = new RotateLeftInterpolation();
+                            }
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        if (interpolation.Step(this, Time.deltaTime * speed))
+        {
+            interpolation = null;
+        }
     }
 }
